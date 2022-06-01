dc5b5dd4dd302dea3ef0e218ec768e780f8f1601
ducduongn <48018619+ducduongn@users.noreply.github.com>
2022-06-02 02:28:09 +0700

    done fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Table
+} from 'react-bootstrap'
 import './App.css';
 
 function App() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const getStudents = async () => {
+
+      const students = await fetchStudents()
+      console.log(students)
+      setStudents(students)
+    }
+
+    getStudents();
+  }, [])
+
+  const fetchStudents = async () => {
+    const res = await fetch("http://localhost:5000/api/students")
+    const data = await res.json()
+
+    return data
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Container>
+      <h3 className="p-3 text-center">Danh sách sinh viên</h3>
+      <Table striped bordered hover >
+        <thead>
+          <tr>
+            <th className= "sid">Số thứ tự</th>
+            <th>Họ và tên</th>
+            <th>Năm sinh</th>
+            <th>Trường đại học</th>
+            <th>Chuyên ngành</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students && students.map((student, key) =>
+            <tr key={student.sid}>
+              <td>{student.sid}</td>
+              <td>{student.full_name}</td>
+              <td>{student.year_of_birth}</td>
+              <td>{student.university}</td>
+              <td>{student.major}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
