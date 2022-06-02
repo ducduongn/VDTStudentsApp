7a5314d2ae675b3dc7e16c3f8614b1298af1bb59
ducduongn <48018619+ducduongn@users.noreply.github.com>
2022-06-02 12:31:36 +0700

    done

--- backend/app.py
+++ backend/app.py
@@ -15,22 +15,22 @@ cors = CORS(app)
 api = Api(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-# app.config['MONGODB_SETTINGS'] = {
-#     'host': os.environ['MONGODB_HOST'],
-#     'username': os.environ['MONGODB_USERNAME'],
-#     'password': os.environ['MONGODB_PASSWORD'],
-#     'db': os.environ['MONGODB_DBNAME'],
-#     'authentication_source': os.environ['MONGODB_AUTH_SOURCE'],
-# }
-
 app.config['MONGODB_SETTINGS'] = {
-        'DB': 'student-db',
-        'USERNAME': 'apiuser',
-        'PASSWORD': 'apipassword',
-        'HOST': 'localhost',
-        'PORT': 27017,
-        'authentication_source': 'student-db',
-    }
+    'host': os.environ['MONGODB_HOST'],
+    'username': os.environ['MONGODB_USERNAME'],
+    'password': os.environ['MONGODB_PASSWORD'],
+    'db': os.environ['MONGODB_DBNAME'],
+    'authentication_source': os.environ['MONGODB_AUTH_SOURCE'],
+}
+
+# app.config['MONGODB_SETTINGS'] = {
+#         'DB': 'student-db',
+#         'USERNAME': 'apiuser',
+#         'PASSWORD': 'apipassword',
+#         'HOST': 'localhost',
+#         'PORT': 27017,
+#         'authentication_source': 'student-db',
+#     }
 
 initialize_db(app)
 initialize_routes(api)
